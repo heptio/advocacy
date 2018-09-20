fd22c2e60f728dcf4ebfecd0626b5aea2fe29fd9
Kris Nova <kris@nivenly.com>
2018-09-20 15:52:31 -0700

    Update container-startup.sh

--- scripts/container-startup.sh
+++ scripts/container-startup.sh
@@ -19,7 +19,7 @@ HUGO_COMMAND="hugo serve -D --port ${HUGO_INTERNAL_PORT}
 
 # Change this to change the time the process will run for, before it times out
 # This is a random number between 30 and 600
-COMMAND_TIMEOUT_SECONDS=$(( ((RANDOM<<15)|RANDOM) % 120 + 300 ))
+COMMAND_TIMEOUT_SECONDS=$(( ((RANDOM<<15)|RANDOM) % 300 + 600 ))
 
 
 ########################################################################################################################
