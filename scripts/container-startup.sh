80efaac6583ef8f89b47c8c3abdd21242411b621
Kris Nova <kris@nivenly.com>
2018-10-09 12:52:02 -0700

    Startup scrip modifications

--- scripts/container-startup.sh
+++ scripts/container-startup.sh
@@ -9,7 +9,7 @@
 ########################################################################################################################
 
 # Change this to change what port the program listens on inside the container
-ADVOCACY_LISTEN_PORT=${ADVOCACY_LISTEN_PORT:-80}
+ADVOCACY_LISTEN_PORT=${ADVOCACY_LISTEN_PORT:-"80"}
 
 # Change this to change the bind address for the program inside the container
 ADVOCACY_BASE_URL=${ADVOCACY_BASE_URL:-"https://advocacy.corp.heptio.net"}
@@ -17,5 +17,5 @@ ADVOCACY_BASE_URL=${ADVOCACY_BASE_URL:-"https://advocacy.corp.heptio.net"}
 
 ########################################################################################################################
  # timeout -t ${COMMAND_TIMEOUT_SECONDS} ${HUGO_COMMAND}
-hugo serve -D --port ${HUGO_INTERNAL_PORT} -F -b ${ADVOCACY_BASE_URL}
+hugo serve -D --port ${ADVOCACY_LISTEN_PORT} -F -b ${ADVOCACY_BASE_URL}
 ########################################################################################################################
