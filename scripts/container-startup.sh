cd4a96148469d395e90379fb7ca3549d4811a0b2
Kris Nova <kris@nivenly.com>
2018-10-09 13:44:55 -0700

    Adding static site

--- scripts/container-startup.sh
+++ scripts/container-startup.sh
@@ -9,7 +9,7 @@
 ########################################################################################################################
 
 # Change this to change what port the program listens on inside the container
-ADVOCACY_LISTEN_PORT=${ADVOCACY_LISTEN_PORT:-"80"}
+ADVOCACY_LISTEN_PORT=${ADVOCACY_LISTEN_PORT:-"443"}
 
 # Change this to change the bind address for the program inside the container
 ADVOCACY_BASE_URL=${ADVOCACY_BASE_URL:-"https://advocacy.corp.heptio.net"}
